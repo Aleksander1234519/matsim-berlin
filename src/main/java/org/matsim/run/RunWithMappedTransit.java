f42c7681a214b48020c6e1a9e2ad895f05b6bd2c
Aleksander1234519 <aleksanderpabis@gmail.com>
2024-09-11 00:16:09 +0200

    Prepared sampling

--- src/main/java/org/matsim/run/RunWithMappedTransit.java
+++ src/main/java/org/matsim/run/RunWithMappedTransit.java
@@ -7,17 +7,20 @@ import org.matsim.core.config.Config;
 /**
  * Run the {@link OpenBerlinScenario} with default configuration.
  */
-public final class RunWithMappedTransit {
+public final class RunWithMappedTransit extends MATSimApplication {
 
-	private RunWithMappedTransit() {
+	@Override
+	protected Config prepareConfig(Config config) {
+		config.qsim().setPcuThresholdForFlowCapacityEasing(0.3); // TODO Test this
+		return config;
 	}
 
 	public static void main(String[] args) {
-		// TODO Set iterations to 500
+		// TODO Set iterations to 200
 		// TODO Set population to 10pct
+		// TODO Set pt pce to 10pct
 		Config c = new Config();
 		String[] mappedTransitArgs = new String[]{"run", "--config=./input/v6.0-10pct-mapped/berlin-v6.0-mapped.config.xml"};
 		MATSimApplication.run(OpenBerlinScenario.class, mappedTransitArgs);
 	}
-
 }
